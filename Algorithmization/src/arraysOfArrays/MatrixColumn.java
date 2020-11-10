c69c40f6901ee57bc8a074e52856d4995eee0a4d
AlexB <alex25091986@gmail.com>
2020-11-10 17:18:21 +0300

    update

--- Algorithmization/src/arraysOfArrays/MatrixColumn.java
+++ Algorithmization/src/arraysOfArrays/MatrixColumn.java
@@ -4,7 +4,7 @@ package arraysOfArrays;
 public class MatrixColumn {
     public static void main(String[] args) {
         int[][] array = {
-                {9, 8, 7},
+                {10, 8, 7},
                 {6, 5, 4},
                 {3, 2, 1}
         };
